4845bca7ef9afc10cbd3abc309ff61a83350a529
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2021-12-08 17:05:14 +0000

    Added header file for types

---
+++ src/types.h
@@ -0,0 +1,9 @@
+#include <eigen3/Eigen/Core>
+
+namespace thames::types {
+
+    // Define vector types
+    typedef Eigen::Matrix<double, 1, 3> Vector3;
+    typedef Eigen::Matrix<double, 1, 6> Vector6;
+
+}
