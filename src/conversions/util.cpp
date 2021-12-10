934ec668cbe6efedf71215a88a3061e28516cdf6
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2021-12-10 13:56:53 +0000

    Formatting for readability

--- src/conversions/util.cpp
+++ src/conversions/util.cpp
@@ -12,9 +12,9 @@ namespace thames::conversions::util {
         Matrix33 rot;
 
         // Populate rotation matrix
-        rot(0, 0) = 1.0f; rot(0, 1) = 0.0f; rot(0, 2) = 0.0f;
-        rot(1, 0) = 0.0f; rot(1, 1) = cos(ang); rot(1, 2) = -sin(ang);
-        rot(2, 0) = 0.0f; rot(2, 1) = sin(ang); rot(2, 2) = cos(ang);
+        rot(0, 0) = 1.0f;   rot(0, 1) = 0.0f;       rot(0, 2) = 0.0f;
+        rot(1, 0) = 0.0f;   rot(1, 1) = cos(ang);   rot(1, 2) = -sin(ang);
+        rot(2, 0) = 0.0f;   rot(2, 1) = sin(ang);   rot(2, 2) = cos(ang);
 
         // Return rotation matrix
         return rot;
@@ -27,9 +27,9 @@ namespace thames::conversions::util {
         Matrix33 rot;
 
         // Populate rotation matrix
-        rot(0, 0) = cos(ang); rot(0, 1) = 0.0f; rot(0, 2) = sin(ang);
-        rot(1, 0) = 0.0f; rot(1, 1) = 1.0f; rot(1, 2) = 0.0f;
-        rot(2, 0) = -sin(ang); rot(2, 1) = 0.0f; rot(2, 2) = cos(ang);
+        rot(0, 0) = cos(ang);   rot(0, 1) = 0.0f;   rot(0, 2) = sin(ang);
+        rot(1, 0) = 0.0f;       rot(1, 1) = 1.0f;   rot(1, 2) = 0.0f;
+        rot(2, 0) = -sin(ang);  rot(2, 1) = 0.0f;   rot(2, 2) = cos(ang);
 
         // Return rotation matrix
         return rot;
@@ -42,9 +42,9 @@ namespace thames::conversions::util {
         Matrix33 rot;
 
         // Populate rotation matrix
-        rot(0, 0) = cos(ang); rot(0, 1) = -sin(ang); rot(0, 2) = 0.0f;
-        rot(1, 0) = sin(ang); rot(1, 1) = cos(ang); rot(1, 2) = 0.0f;
-        rot(2, 0) = 0.0f; rot(2, 1) = 0.0f; rot(2, 2) = 1.0f;
+        rot(0, 0) = cos(ang);   rot(0, 1) = -sin(ang);  rot(0, 2) = 0.0f;
+        rot(1, 0) = sin(ang);   rot(1, 1) = cos(ang);   rot(1, 2) = 0.0f;
+        rot(2, 0) = 0.0f;       rot(2, 1) = 0.0f;       rot(2, 2) = 1.0f;
 
         // Return rotation matrix
         return rot;
