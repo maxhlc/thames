7292ae178c7815d29e84ae8a08ad10643e22cc9e
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2021-12-10 09:46:56 +0000

    Split to optimise file, correction for root

---
+++ src/util/optimise.cpp
@@ -0,0 +1,37 @@
+#include <functional>
+#include <math.h>
+
+#include "optimise.h"
+
+namespace thames::util::optimise{
+
+    double golden_section_search(std::function<double (double)> func, double a, double b){
+        // TODO: documentation
+
+        // Set parameters
+        const double gr = 0.5f*(sqrt(5.0f) + 1);
+        const double tol = 1e-14;
+
+        // Calculate test points
+        double c = b - (b - a)/gr;
+        double d = a + (b - a)/gr;
+
+        // Iterate until convergence
+        while (abs(b - a) > tol) {
+            // Choose test points based on function values
+            if (func(c) < func(d)) {
+                b = d;
+            } else {
+                a = c;
+            }
+
+            // Recompute test points
+            c = b - (b - a)/gr;
+            d = a + (b - a)/gr;           
+        }
+
+        // Return converged value
+        return (0.5f*(a + b));
+    }
+
+}
--- src/util/root.cpp
+++ src/util/root.cpp
@@ -2,36 +2,21 @@
 #include <math.h>
 
 #include "root.h"
+#include "optimise.h"
 
 namespace thames::util::root{
 
     double golden_section_search(std::function<double (double)> func, double a, double b){
         // TODO: documentation
 
-        // Set parameters
-        const double gr = 0.5f*(sqrt(5.0f) + 1);
-        const double tol = 1e-14;
+        // Declare function for minimisation (root at minimum of absolute of the function)
+        std::function<double (double)> f = [func](double x) {return abs(func(x));};
 
-        // Calculate test points
-        double c = b - (b - a)/gr;
-        double d = a + (b - a)/gr;
+        // Minimise function using the golden section search
+        double x0 = thames::util::optimise::golden_section_search(f, a, b);
 
-        // Iterate until convergence
-        while (abs(b - a) > tol) {
-            // Choose test points based on function values
-            if (func(c) < func(d)) {
-                b = d;
-            } else {
-                a = c;
-            }
-
-            // Recompute test points
-            c = b - (b - a)/gr;
-            d = a + (b - a)/gr;           
-        }
-
-        // Return converged value
-        return (0.5f*(a + b));
+        // Return root
+        return x0;
     }
 
 }
