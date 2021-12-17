f7710cfe270258fc612f3aa19ffb0b761d730f90
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2021-12-17 14:55:27 +0000

    Initial documentation for constants

--- src/constants/earth.h
+++ src/constants/earth.h
@@ -3,8 +3,13 @@
 
 namespace thames::constants::earth{
 
+    /// Radius of the Earth [km]
     const double radius = 6378.0;
+
+    /// Earth's gravitational parameter [km^3/s^2]
     const double mu = 3.986004414498200E+05;
+
+    /// Earth's J2 term [-]
     const double J2 = 1.082635854E-03;
 
 }
