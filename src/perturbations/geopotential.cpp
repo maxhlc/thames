cfc33b3ef0d5a90f595ccb8473befa436dc465fc
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2021-12-17 15:00:10 +0000

    Initial documentation for perturbations

--- src/perturbations/geopotential.cpp
+++ src/perturbations/geopotential.cpp
@@ -5,8 +5,6 @@ using namespace thames::types;
 namespace thames::perturbations::geopotential{
 
     Vector3 J2_acceleration(double t, Vector3 R, Vector3 V, double mu, double J2, double radius){
-        // TODO: documentation
-
         // Extract position components
         double x = R[0], y = R[1], z = R[2];
 
@@ -28,8 +26,6 @@ namespace thames::perturbations::geopotential{
     }
 
     double J2_potential(double t, Vector3 R, double mu, double J2, double radius){
-        // TODO: documentation
-
         // Extract position components
         double x = R[0], y = R[1], z = R[2];
 
@@ -47,8 +43,6 @@ namespace thames::perturbations::geopotential{
     }
 
     double J2_dpotential(double t, Vector3 R, Vector3 V){
-        // TODO: documentation
-
         // Return zero change
         return 0.0;
     }
--- src/perturbations/geopotential.h
+++ src/perturbations/geopotential.h
@@ -7,10 +7,39 @@ using namespace thames::types;
 
 namespace thames::perturbations::geopotential{
 
+    /**
+     * @brief Calculate perturbing acceleration resulting from the J2 term.
+     * 
+     * @param t Current physical time.
+     * @param R Position vector.
+     * @param V Velocity vector.
+     * @param mu Central body gravitational parameter.
+     * @param J2 Central body J2 term.
+     * @param radius Central body radius.
+     * @return Vector3 Perturbing acceleration.
+     */
     Vector3 J2_acceleration(double t, Vector3 R, Vector3 V, double mu, double J2, double radius);
 
+    /**
+     * @brief Calculate perturbing potential resulting from the J2 term.
+     * 
+     * @param t Current physical time.
+     * @param R Position vector.
+     * @param mu Central body gravitational parameter.
+     * @param J2 Central body J2 term.
+     * @param radius Central body radius.
+     * @return double Perturbing potential.
+     */
     double J2_potential(double t, Vector3 R, double mu, double J2, double radius);
 
+    /**
+     * @brief Calculate time derivative of the perturbing potential resulting from the J2 term.
+     * 
+     * @param t Current physical time.
+     * @param R Position vector.
+     * @param V Velocity vector.
+     * @return double Time derivative of the perturbing potential.
+     */
     double J2_dpotential(double t, Vector3 R, Vector3 V);
 
 }
