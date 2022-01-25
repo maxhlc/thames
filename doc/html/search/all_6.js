127f7baef8af5ae85a629b6e568fce53e8279e80
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2022-01-25 16:01:14 +0000

    Documentation update

--- doc/html/search/all_6.js
+++ doc/html/search/all_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['j2_46',['J2',['../classthames_1_1perturbations_1_1geopotential_1_1J2.html',1,'thames::perturbations::geopotential::J2&lt; T &gt;'],['../classthames_1_1perturbations_1_1geopotential_1_1J2.html#ae9f60d33036757413171ec76b8327c75',1,'thames::perturbations::geopotential::J2::J2()'],['../namespacethames_1_1constants_1_1earth.html#a046a5d85b76e60f10013af278a6b9e23',1,'thames::constants::earth::J2()']]],
+  ['j2_46',['J2',['../classthames_1_1perturbations_1_1geopotential_1_1J2.html',1,'thames::perturbations::geopotential::J2&lt; T &gt;'],['../classthames_1_1perturbations_1_1geopotential_1_1J2.html#aef1f4b8e4fff458758900897f6a52169',1,'thames::perturbations::geopotential::J2::J2()'],['../namespacethames_1_1constants_1_1earth.html#a046a5d85b76e60f10013af278a6b9e23',1,'thames::constants::earth::J2()']]],
   ['j2_2ecpp_47',['J2.cpp',['../J2_8cpp.html',1,'']]],
   ['j2_2eh_48',['J2.h',['../J2_8h.html',1,'']]]
 ];
--- doc/html/search/functions_5.js
+++ doc/html/search/functions_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['j2_195',['J2',['../classthames_1_1perturbations_1_1geopotential_1_1J2.html#ae9f60d33036757413171ec76b8327c75',1,'thames::perturbations::geopotential::J2']]]
+  ['j2_195',['J2',['../classthames_1_1perturbations_1_1geopotential_1_1J2.html#aef1f4b8e4fff458758900897f6a52169',1,'thames::perturbations::geopotential::J2']]]
 ];
