975ed23b22ad54063e612d1f4e3cd4b535dceba2
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2022-04-06 16:08:58 +0100

    Updated batch scripts

--- batch/thames_batch.py
+++ batch/thames_batch.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python3
 
+# Built-in packages
+from datetime import datetime
+from io import StringIO
+import multiprocessing
+import os
+import random
+import time
+
+# Third-party packages
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import os
-from io import StringIO
 from smt.sampling_methods import LHS
 from tqdm import tqdm
-import time
-from datetime import datetime
-import multiprocessing
 
 def thames_input(filepath, tstart, tend, points, scid, atol, rtol, statetype, degree):
+    # Open filepath for input file
     with open(filepath, "w") as f:
+        # Write header
         f.write(f"{tstart}, {tend}, {scid}, {statetype}, {degree}, {atol}, {rtol}\n")
+
+        # Iterate through points, and write to input file
         for point in points:
             f.write(f"{point[0]:+8e}, {point[1]:+8e}, {point[2]:+8e}, {point[3]:+8e}, {point[4]:+8e}, {point[5]:+8e}\n")
 
 def thames_output(filepath):
+    # Open filepath for output file
     with open(filepath, "r") as f:
+        # Read file
         lines = f.readlines()
 
+    # Read output states, and convert to a Numpy array
     states = " ".join(lines[:-1])
     states = np.genfromtxt(StringIO(states), delimiter=",")
 
+    # Split footer by comma
     param = lines[-1].split(",")
+
+    # Read footer parameters
     tstart = float(param[0])
     tend = float(param[1])
     scid = int(param[2])
@@ -33,6 +47,7 @@ def thames_output(filepath):
     atol = float(param[5])
     rtol = float(param[6])
 
+    # Return parameters and states
     return tstart, tend, scid, states, statetype, degree, atol, rtol 
 
 def thames_run(command, filepathin, filepathout, tstart, tend, states, degree, atol, rtol, statetype=0, scid=0):
@@ -57,58 +72,79 @@ def thames_run(command, filepathin, filepathout, tstart, tend, states, degree, a
     # Return output
     return states_propagated
 
+# Set I/O filepaths
+filepathin = "./batch_in"
+filepathout = "./batch_out"
 
-filepathin = "./test_in.txt"
-filepathout = "./test_out.txt"
-
+# Set propagation start and end time
 tstart = 0.0
-tend = 114000 #5*5400.0
+tend = 5400.0
 
-# RV = np.array([-4102, 5093, -1866, -2.9, -4.3, -5.6])
-# RVunc = np.array([0.1, 0.1, 0.1, 0.01, 0.01, 0.01])
-# RVupper = RV + RVunc
-# RVlower = RV - RVunc
-RVupper = np.array([-4416782.92246406, -5263218.34932045, 2773.68818819729, 7656.46288470595, -6412.88169544488, 0.882777270214449])/1000.0
-RVlower = np.array([-4422432.26516413, -5270764.01693497, -2693.42920650658, 7641.65569328887, -6424.59052100732, -0.819487850875413])/1000.0
+# Set initial states and uncertainties
+RV = np.array([-4102, 5093, -1866, -2.9, -4.3, -5.6])
+RVunc = np.array([0.1, 0.1, 0.1, 0.01, 0.01, 0.01])
+
+# Generate initial state boundaries
+RVupper = RV + RVunc
+RVlower = RV - RVunc
 RVbound = np.vstack([RVlower, RVupper]).T
+
+# Sample initial boundaries
 sampling = LHS(xlimits=RVbound)
 states = sampling(10**3)
 
-command = ["thames_cowell_discrete_point"] #, "thames_geqoe_discrete_point"]
+# Set desired point, and polynomial commands
+command = ["thames_cowell_discrete_point"]
 command_poly = ["thames_cowell_polynomial_point", "thames_geqoe_polynomial_point"]
-degree = range(2, 11, 2)
-tstep = np.array([15, 30, 45, 60, 90, 120, 180, 240, 360, 480, 1200, 1920, 2880, 3840, 5760, 7680])
-# atol = 10.0**np.arange(-14, -9, 1)
-atol = tstep
 
-perms = [(commandi, degreei, atoli) for commandi in command for degreei in [0] for atoli in [10**(-13)]]
+# Set desired polynomial degrees
+degree = range(2, 11, 2)
+
+# Set desired solver tolerances
+atol = 10.0**np.arange(-14, -9, 1)
+
+# Generate permutations for point, and polynomial commands
+perms = [(commandi, degreei, atoli) for commandi in command for degreei in [0] for atoli in atol]
 perms_poly = [(commandi, degreei, atoli) for commandi in command_poly for degreei in degree for atoli in atol]
 perms.extend(perms_poly)
-perms.reverse()
-print(perms)
 
+# Shuffle permutations
+random.shuffle(perms)
+
+# Declare empty list to store propagated states
 states_propagated = []
 
 def worker(perm):
+    # Unpack parameters
     commandi, degreei, atoli = perm
+
+    # Retrieve worker name
     name = multiprocessing.current_process().name
-    statei = thames_run(commandi, f"{filepathin}{name}", f"{filepathout}{name}", tstart, tend, states, degreei, atoli, atoli)
+
+    # Execute THAMES, appending the worker name to the filepaths to avoid interaction between the workers
+    statei = thames_run(commandi, f"{filepathin}_{name}.txt", f"{filepathout}_{name}.txt", tstart, tend, states, degreei, atoli, atoli)
+
+    # Return propagated state
     return statei
 
-# for perm in tqdm(perms):
-#     statei = worker(perm)
-#     states_propagated.append(statei)
-
+# Create worker pool
 with multiprocessing.Pool() as pool:
+    # Create progress bar
     with tqdm(total=len(perms)) as pbar:
+        # Iterate through permutations
         for statei in pool.imap_unordered(worker, perms):
+            # Store propagated state
             states_propagated.append(statei)
+
+            # Update progress bar
             pbar.update(1)
 
-now = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-pkl_path = f"sweep_{now}.pkl"
+# Calculate current time for output file name
+now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+csv_path = f"sweep_{now}.csv"
 
+# Generate pandas table of propagated states
 states_propagated = pd.concat(states_propagated)
-states_propagated.to_pickle(pkl_path)
-print(states_propagated.head())
-print(states_propagated.tail())
+
+# Save propagated states
+states_propagated.to_csv(csv_path)
