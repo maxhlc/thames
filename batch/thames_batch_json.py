91d3d29f222eb1ae4501a725bb5022ade6308e2c
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2022-06-27 12:24:07 +0100

    Major update

--- batch/thames_batch_json.py
+++ batch/thames_batch_json.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import copy
 import dataclasses
 import datetime
 import os
+import statistics
 from typing import List
 import itertools
 import multiprocessing
@@ -11,6 +13,9 @@ import dataclasses_json
 import numpy as np
 import smt.sampling_methods
 import tqdm
+import pandas as pd
+
+BATCH_PATH = os.path.dirname(os.path.realpath(__file__))
 
 @dataclasses_json.dataclass_json
 @dataclasses.dataclass
@@ -57,6 +62,7 @@ class PropagatorParameters:
     isNonDimensional: bool
     isFixedStep: bool
     intermediateOutput: bool
+    timeStepIntermediate: float
     timeStep: float
     absoluteTolerance: float
     relativeTolerance: float
@@ -77,6 +83,11 @@ class StateParameters:
 
 @dataclasses_json.dataclass_json
 @dataclasses.dataclass
+class ExecutionStatistics:
+    propagationTime: float
+
+@dataclasses_json.dataclass_json
+@dataclasses.dataclass
 class Parameters:
     metadata: Metadata
     spacecraft: SpacecraftParameters
@@ -84,6 +95,7 @@ class Parameters:
     propagator: PropagatorParameters
     polynomial: PolynomialParameters
     states: List[StateParameters]
+    statistics: ExecutionStatistics
 
 @dataclasses_json.dataclass_json
 @dataclasses.dataclass
@@ -113,7 +125,8 @@ def parameter_permutations(parameter_lists: dict, perturbation: PerturbationPara
             perturbation,
             PropagatorParameters(*propagator), 
             PolynomialParameters(*polynomial),
-            [states]
+            [states],
+            ExecutionStatistics(0.0)
             )
         for metadata, spacecraft, propagator, polynomial in permutations
     ]
@@ -137,7 +150,7 @@ def save(filepath: str, parameters: Parameters) -> None:
     # Open file
     with open(filepath, "w") as fid:
         # Output parameters in JSON format
-        fid.write(parameters.to_json())
+        fid.write(parameters.to_json(indent=4))
 
 def load(filepath: str) -> Parameters:
     # Read file
@@ -168,13 +181,10 @@ def worker(parameter: Parameters) -> Parameters:
     # Find process name
     name = multiprocessing.current_process().name
 
-    # Define I/O file directory, and executable path
-    filedir = "batch"
-    command = "../bin/thames_main"
-
     # Generate I/O filepaths
-    filepathin = f"{filedir}_{name}_in.json"
-    filepathout = f"{filedir}_{name}_out.json"
+    command = os.path.join(BATCH_PATH, "..", "bin", "thames_main")
+    filepathin = os.path.join(BATCH_PATH, f"batch_{name}_in.json")
+    filepathout =  os.path.join(BATCH_PATH, f"batch_{name}_out.json")
 
     # Execute THAMES
     parameters_propagated = run(command, parameter, filepathin, filepathout)
@@ -183,6 +193,9 @@ def worker(parameter: Parameters) -> Parameters:
     return parameters_propagated
 
 def main():
+    # Set parameters
+    rms_limits = [(0, 3), (3, 6)]
+
     # Set initial states and uncertainties
     RV = np.array([-4102, 5093, -1866, -2.9, -4.3, -5.6])
     RVunc = np.array([0.1, 0.1, 0.1, 0.01, 0.01, 0.01])
@@ -214,25 +227,39 @@ def main():
         "propagator": {
             "startTime": [0],
             "endTime": [5400],
-            "equations": ["Cowell", "GEqOE"],
+            "equations": ["Cowell"],
             "isNonDimensional" : [True],
             "isFixedStep": [False],
             "intermediateOutput": [False],
+            "timeStepIntermediate": [30],
             "timeStep": [30],
             "absoluteTolerance": [1e-14],
             "relativeTolerance": [1e-14]
         },
         "polynomial": {
             "isEnabled": [False],
-            "type": ["Taylor"],
-            "maxDegree": [2, 4, 6, 8, 10]
+            "type": [""],
+            "maxDegree": [0]
         },
     }
+
+    # Generate parameter sets for polynomial propagations
+    parameter_lists_polynomial = copy.deepcopy(parameter_lists)
+    # Set propagator parameters
+    parameter_lists_polynomial["propagator"]["equations"] = ["GEqOE"]
+    parameter_lists_polynomial["propagator"]["isNonDimensional"] = [True, False] 
+    parameter_lists_polynomial["propagator"]["isFixedStep"] = [False]
+    parameter_lists_polynomial["propagator"]["absoluteTolerance"] = [1e-13]
+    parameter_lists_polynomial["propagator"]["relativeTolerance"] = [1e-13]
+    # Set polynomial parameters
+    parameter_lists_polynomial["polynomial"]["isEnabled"] = [True]
+    parameter_lists_polynomial["polynomial"]["type"] = ["Taylor"]
+    parameter_lists_polynomial["polynomial"]["maxDegree"] = [6]
 
     # Set constant parameters (i.e. perturbation model, and states)
     perturbation = PerturbationParameters(
         GeopotentialPerturbationParameters(True, "J2", 0, 0),
-        AtmospherePerturbationParameters(True, "USSA76")
+        AtmospherePerturbationParameters(False, "Wertz")
     )
     states = StateParameters(
         0,
@@ -241,7 +268,16 @@ def main():
     )
 
     # Generate parameter permutations
-    parameters = parameter_permutations(parameter_lists, perturbation, states)
+    parameters = [
+        *parameter_permutations(parameter_lists, perturbation, states),
+        *parameter_permutations(parameter_lists_polynomial, perturbation, states)
+    ]
+
+    # Remove permutations where the absolute tolerance does not match the relative tolerance
+    parameters = [param for param in parameters if param.propagator.absoluteTolerance == param.propagator.relativeTolerance]
+
+    # Sort permutations
+    parameters = sorted(parameters, key=lambda x: (x.propagator.timeStep, x.propagator.absoluteTolerance, -x.polynomial.maxDegree))
 
     # Iterate through permutations
     parameters_propagated = list()
@@ -256,18 +292,87 @@ def main():
 
                 # Update progress bar
                 pbar.update(1)
-    
-    # Create set of parameters
-    parameters_propagated_set = ParametersSet(parameters_propagated)
+
+    # Generate tables of results
+    parameters_propagated_tables = list()
+    for ip in parameters_propagated:
+        # Declare table for each propagation
+        ip_table = pd.DataFrame()
+
+        # Iterate through states
+        for state in ip.states:
+            # Declare table for each set of states
+            state_table = pd.DataFrame()
+
+            # Generate labels
+            state_labels = [f"state_{stri}" for stri in range(0, len(state.states[0]))]
+
+            # Add states, datetime, and statetype to the table
+            state_table[state_labels] = np.array(state.states)
+            state_table["datetime"] = state.datetime
+            state_table["statetype"] = state.statetype
+
+            # Append data to overall propagation table
+            ip_table = pd.concat((ip_table, state_table))
+
+        # Add metadata
+        propagator = ip.propagator.to_dict()
+        polynomial = ip.polynomial.to_dict()
+        statistics = ip.statistics.to_dict()
+        ip_table[list(propagator.keys())] = list(propagator.values())
+        ip_table[list(polynomial.keys())] = list(polynomial.values())
+        ip_table[list(statistics.keys())] = list(statistics.values())
+
+        # Store table
+        parameters_propagated_tables.append(ip_table)
+
+    # Pick reference solution
+    ref_solution = sorted([iparam for iparam in parameters_propagated_tables if not iparam["isEnabled"].all()], key=lambda x: (x["absoluteTolerance"].iloc[0], x["timeStep"].iloc[0]))[0]
+
+    # Set pivot parameters
+    pivot_parameters = []
+    pivot_parameters.append("datetime")
+    pivot_parameters.extend([param for param in list(PropagatorParameters.__dataclass_fields__)])
+    pivot_parameters.extend([param for param in list(PolynomialParameters.__dataclass_fields__)])
+    pivot_parameters.extend([param for param in list(ExecutionStatistics.__dataclass_fields__)])
+
+    # Calculate bulk statistics
+    bulk_table = pd.DataFrame()
+    for table in tqdm.tqdm(parameters_propagated_tables):
+        # Calculate deltas in state variables
+        state_cols = [col for col in table.columns if "state_" in col]
+        dstate_cols = [f"delta_{col}" for col in state_cols]
+        table[dstate_cols] = table[state_cols] - ref_solution[state_cols]
+
+        # Iterate through slices
+        for slice in rms_limits:
+            # Define related columns
+            state_cols = [f"delta_state_{col}" for col in range(*slice)]
+            dstate_cols = f"delta_state_{slice[0]}_{slice[-1]-1}"
+
+            # Iterate through times
+            for t in np.unique(table["datetime"]):
+                # Calculate slice magnitude
+                table.loc[(table["datetime"] == t), dstate_cols] = np.linalg.norm(table.loc[(table["datetime"] == t), state_cols], axis=1)
+
+        # Condense, and append to bulk table
+        bulk_table = pd.concat((
+            bulk_table,
+            pd.pivot_table(
+                table,
+                values=[f"delta_state_{slice[0]}_{slice[-1]-1}" for slice in rms_limits],
+                index=pivot_parameters,
+                aggfunc=lambda x: np.sqrt(np.mean(x**2))
+                )
+            )
+        )
 
     # Calculate current time for output file name
     now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    filepathbatch = f"batch_{now}.json"
+    csv_path = os.path.join(BATCH_PATH, f"batch_{now}.csv") 
 
-    # Save parameter sets
-    with open(filepathbatch, "w") as fid:
-        # Output parameters in JSON format
-        fid.write(parameters_propagated_set.to_json())
+    # Save bulk table
+    bulk_table.to_csv(csv_path)
 
 if __name__ == "__main__":
     main()
