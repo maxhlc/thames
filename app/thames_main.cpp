5bb97249f6b7c71c7d42310c8e9d6e8369f262d4
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2022-08-24 12:48:41 +0100

    Add Wertz-P1

--- app/thames_main.cpp
+++ app/thames_main.cpp
@@ -52,6 +52,10 @@ thames::settings::Parameters<T> propagate(const thames::settings::Parameters<T>&
             auto atmospheremodel = std::make_shared<thames::perturbations::atmosphere::models::WertzAtmosphereModel<T>>();
             auto atmosphereperturbation = std::make_shared<thames::perturbations::atmosphere::drag::Drag<T>>(radius, w, parameters.spacecraft.Cd, parameters.spacecraft.dragArea, parameters.spacecraft.mass, atmospheremodel, factors);
             perturbation->add_model(atmosphereperturbation);
+        } else if (parameters.perturbation.atmosphere.model == "Wertz-P1") {
+            auto atmospheremodel = std::make_shared<thames::perturbations::atmosphere::models::WertzP1AtmosphereModel<T>>();
+            auto atmosphereperturbation = std::make_shared<thames::perturbations::atmosphere::drag::Drag<T>>(radius, w, parameters.spacecraft.Cd, parameters.spacecraft.dragArea, parameters.spacecraft.mass, atmospheremodel, factors);
+            perturbation->add_model(atmosphereperturbation);   
         } else if (parameters.perturbation.atmosphere.model == "Wertz-P5") {
             auto atmospheremodel = std::make_shared<thames::perturbations::atmosphere::models::WertzP5AtmosphereModel<T>>();
             auto atmosphereperturbation = std::make_shared<thames::perturbations::atmosphere::drag::Drag<T>>(radius, w, parameters.spacecraft.Cd, parameters.spacecraft.dragArea, parameters.spacecraft.mass, atmospheremodel, factors);
@@ -146,6 +150,10 @@ thames::settings::Parameters<T> propagate(const thames::settings::Parameters<T>&
             perturbation->add_model(atmosphereperturbation);
         } else if (parameters.perturbation.atmosphere.model == "Wertz") {
             auto atmospheremodel = std::make_shared<thames::perturbations::atmosphere::models::WertzAtmosphereModelPolynomial<T, P>>();
+            auto atmosphereperturbation = std::make_shared<thames::perturbations::atmosphere::drag::DragPolynomial<T, P>>(radius, w, parameters.spacecraft.Cd, parameters.spacecraft.dragArea, parameters.spacecraft.mass, atmospheremodel, factors);
+            perturbation->add_model(atmosphereperturbation);
+        } else if (parameters.perturbation.atmosphere.model == "Wertz-P1") {
+            auto atmospheremodel = std::make_shared<thames::perturbations::atmosphere::models::WertzP1AtmosphereModelPolynomial<T, P>>();
             auto atmosphereperturbation = std::make_shared<thames::perturbations::atmosphere::drag::DragPolynomial<T, P>>(radius, w, parameters.spacecraft.Cd, parameters.spacecraft.dragArea, parameters.spacecraft.mass, atmospheremodel, factors);
             perturbation->add_model(atmosphereperturbation);
         } else if (parameters.perturbation.atmosphere.model == "Wertz-P5") {
